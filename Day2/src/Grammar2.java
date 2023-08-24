91e0938a3e8d4ce146112ac39b75ca784e2b9385
Hoshi <whdgus9655@naver.com>
2023-08-24 21:51:55 +0900

    2자바 강의 commit

---
+++ Day2/src/Grammar2.java
@@ -0,0 +1,22 @@
+
+public class Grammar2 { // 클래스
+
+	public void 멍때리다() {
+		//콜라
+		//밥먹고
+		//라면
+		//멍
+		
+	}
+	
+	public void 잠자다() {
+		
+	}
+	
+	public static void main(String [] args) {
+	 
+		System.out.println();
+	 
+ }
+	
+}
